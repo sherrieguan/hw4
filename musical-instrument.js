3a5427cf52c69d6ff63ded4e35599f8ef7cf1de9
sherrieguan <35797658+sherrieguan@users.noreply.github.com>
2018-02-18 02:53:02 -0800

    Update and rename visual-feedback.js to musical-instrument.js

--- musical-instrument.js
+++ musical-instrument.js
@@ -1,3 +1,15 @@
+// I created a musical instrument to play my all time favorite song, Shelter by Porter Robinson and Madeon
+// The main melody and intro use five notes, C, A, G, D, and E.
+// When playing all keys A, S, D, F, and G on your keyboard, the Shelter symbol appears!
+// If you aren't familiar with the song, refer to the official audio here: https://www.youtube.com/watch?v=HQnC1UHBvWA
+// How to play the melody from Shelter using your keyboard:
+// Play: A S D A
+// A S D F ... F A
+// A S D A
+// G G A F G A F G A F G A F A
+// and repeat once
+// Songsheet I used for reference: https://musescore.com/bomb_and_kou/shelter
+
 var freqA = 261;
 var freqS = 440;
 var freqD = 392;
@@ -92,6 +104,14 @@ function draw() {
     fill(0);
     noStroke();
     ellipse(50, 50, 75, 75);
+    fill(0, 153, 115);
+    triangle(50, 20, 65, 35, 35, 35);
+    fill(0);
+    triangle(50, 26, 65, 41, 35, 41);
+    fill(0, 153, 115);
+    triangle(50, 80, 65, 65, 35, 65);
+    fill(0);
+    triangle(50, 74, 65, 59, 35, 59);
   }
   if (playingD) {
     fill(0, 153, 115);
@@ -105,14 +125,14 @@ function draw() {
   if (playingF) {
     fill(0, 153, 115);
     noStroke();
-    rect(18, 40, 15, 8);
-    rect(18, 55, 15, 8);
+    rect(18, 43, 15, 5);
+    rect(18, 53, 15, 5);
   }
   if (playingG) {
     fill(0, 153, 115);
     noStroke();
-    rect(67, 40, 15, 8);
-    rect(67, 55, 15, 8);
+    rect(67, 43, 15, 5);
+    rect(67, 53, 15, 5);
   }
 }
 
